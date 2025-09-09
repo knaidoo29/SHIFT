5ec6c2082d6f0347db10ae5099daf5b664fa5db7
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2025-09-09 12:25:29 +0100

    testing single mpiclass test function

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 coverage erase
 coverage run -m pytest tests/normal_tests
-mpirun -n 4 coverage run -m pytest tests/mpi_tests
+mpirun -n 2 coverage run -m pytest tests/mpi_tests
 coverage combine
 coverage report -m
